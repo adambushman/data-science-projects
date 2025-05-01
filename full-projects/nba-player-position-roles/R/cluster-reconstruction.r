9ec275560c7b950eeaaf7dc48b6cfaf8e2852001
Adam Bushman <adam.bushman1@gmail.com>
2025-04-30 23:03:57 -0600

    Altered code for reproducible clusters

---
+++ full-projects/nba-player-position-roles/R/cluster-reconstruction.r
@@ -0,0 +1,340 @@
+# Your text as a character vector
+players1 <- c(
+    "Russell Westbrook", "Draymond Green", "Kyle Anderson",
+    "Cole Anthony", "Josh Giddey", "T.J. McConnell",
+    "Tre Jones", "Killian Hayes", "Talen Horton-Tucker",
+    "Jalen Suggs", "Jaden Ivey", "Ben Simmons",
+    "Theo Maledon", "Dennis Smith Jr.", "Ricky Rubio",
+    "Markelle Fultz", "Ish Smith", "R.J. Hampton",
+    "Kris Dunn", "Derrick Rose", "Dalano Banton",
+    "Tomas Satoransky", "Scoot Henderson", "Elfrid Payton",
+    "Jordan Goodwin", "Josh Christopher", "Saben Lee",
+    "Trent Forrest", "Blake Wesley", "Keon Johnson",
+    "Rajon Rondo", "Vasilije Micic", "Brad Wanamaker",
+    "Daishen Nix", "Jared Butler", "Scotty Pippen Jr.",
+    "Brandon Goodwin"
+)
+
+# Create a dataframe
+df1 <- data.frame(
+    player = players1,
+    label = "Versatile Anchor"
+)
+
+# Your text as a character vector
+players2 <- c(
+    "Nikola Vucevic", "Pascal Siakam", "Tobias Harris",
+    "Josh Hart", "Keldon Johnson", "Kyle Kuzma",
+    "Aaron Gordon", "John Collins", "Deni Avdija",
+    "Lauri Markkanen", "Scottie Barnes", "Bobby Portis",
+    "Brook Lopez", "Miles Bridges", "Bruce Brown",
+    "Myles Turner", "Michael Porter Jr.", "Jaren Jackson Jr.",
+    "Rui Hachimura", "Kelly Olynyk", "Naz Reid",
+    "KJ Martin", "Christian Wood", "Jae'Sean Tate",
+    "Jabari Smith Jr.", "Jalen Williams", "Jonathan Kuminga",
+    "Larry Nance Jr.", "Moritz Wagner", "Trey Lyles",
+    "Kevin Love", "Bennedict Mathurin", "Darius Bazley",
+    "Santi Aldama", "Jeremy Sochan", "Jalen Johnson",
+    "Zach Collins", "Hamidou Diallo", "Aleksej Pokusevski",
+    "Keita Bates-Diop", "Chimezie Metu", "Trendon Watford",
+    "JaMychal Green", "Dario Saric", "Isaiah Roby",
+    "Blake Griffin", "Chet Holmgren", "Josh Jackson",
+    "Otto Porter Jr.", "Bol Bol", "Justise Winslow",
+    "Serge Ibaka", "Anthony Gill", "Jaylin Williams",
+    "Nemanja Bjelica", "LaMarcus Aldridge", "Sandro Mamukelashvili",
+    "Paul Millsap", "Eric Paschall", "Duop Reath",
+    "David Nwaba", "Gorgui Dieng", "Frank Kaminsky",
+    "Eugene Omoruyi"
+)
+
+# Create a dataframe
+df2 <- data.frame(
+    player = players2,
+    label = "Versatile Finisher"
+)
+
+# Your text as a character vector
+players3 <- c(
+    "Mikal Bridges", "RJ Barrett", "Terry Rozier",
+    "Gary Trent Jr.", "Saddiq Bey", "Luguentz Dort",
+    "Andrew Wiggins", "Tyler Herro", "Franz Wagner",
+    "Jalen Green", "Jerami Grant", "Dillon Brooks",
+    "Norman Powell", "Bojan Bogdanovic", "Desmond Bane",
+    "Bogdan Bogdanovic", "Devin Vassell", "Eric Gordon",
+    "De'Andre Hunter", "Josh Richardson", "Alec Burks",
+    "Klay Thompson", "Seth Curry", "Gordon Hayward",
+    "Marcus Morris Sr.", "Lonnie Walker IV", "Will Barton",
+    "Evan Fournier", "Terrence Ross", "Danilo Gallinari",
+    "Carmelo Anthony", "Malaki Branham", "Jaylen Nowell",
+    "Jordan Nwora", "Shaedon Sharpe", "Rudy Gay",
+    "Chris Duarte", "Furkan Korkmaz", "Brandon Miller",
+    "Kendrick Nunn", "Terence Davis", "Jaden Hardy",
+    "Brandon Boston Jr.", "Dwayne Bacon", "Jeremy Lamb",
+    "AJ Griffin", "Jordan Hawkins", "Duane Washington Jr.",
+    "Denzel Valentine", "GG Jackson II"
+)
+
+# Create a dataframe
+df3 <- data.frame(
+    player = players3,
+    label = "Versatile Engine"
+)
+
+# Your text as a character vector
+players4 <- c(
+    "Jayson Tatum", "Anthony Edwards",
+    "DeMar DeRozan", "Dejounte Murray",
+    "Luka Doncic", "De'Aaron Fox",
+    "Trae Young", "Jalen Brunson",
+    "Devin Booker", "Jaylen Brown",
+    "James Harden", "Darius Garland",
+    "Stephen Curry", "Donovan Mitchell",
+    "Shai Gilgeous-Alexander", "Damian Lillard",
+    "LeBron James", "Zach LaVine",
+    "Jordan Poole", "Chris Paul",
+    "Jimmy Butler", "Brandon Ingram",
+    "Kevin Durant", "Kyrie Irving",
+    "Jordan Clarkson", "Paul George",
+    "Bradley Beal", "Khris Middleton",
+    "Ja Morant", "LaMelo Ball",
+    "Jamal Murray", "Collin Sexton",
+    "Kawhi Leonard", "Paolo Banchero",
+    "Cade Cunningham", "Kevin Porter Jr.",
+    "Cam Thomas", "John Wall"
+)
+
+# Create a dataframe
+df4 <- data.frame(
+    player = players4,
+    label = "Perimeter Engine"
+)
+
+# Your text as a character vector
+players5 <- c(
+    "Jarrett Allen", "Deandre Ayton", "Evan Mobley",
+    "Isaiah Stewart", "Wendell Carter Jr.", "Nic Claxton",
+    "Kevon Looney", "Chris Boucher", "Isaiah Hartenstein",
+    "Jarred Vanderbilt", "Onyeka Okongwu", "Precious Achiuwa",
+    "Dwight Powell", "Drew Eubanks", "Marvin Bagley III",
+    "Brandon Clarke", "Mo Bamba", "Xavier Tillman Sr.",
+    "Jaxson Hayes", "Daniel Theis", "Montrezl Harrell",
+    "Richaun Holmes", "Thaddeus Young", "Jalen Smith",
+    "Nick Richards", "Goga Bitadze", "Paul Reed",
+    "James Wiseman", "Tari Eason", "Taj Gibson",
+    "Khem Birch", "Luke Kornet", "Jabari Walker",
+    "Jock Landale", "Zeke Nnaji", "Thomas Bryant",
+    "Alex Len", "Robin Lopez", "Damian Jones",
+    "Nerlens Noel", "Enes Freedom", "Amen Thompson",
+    "Wenyen Gabriel", "Dewayne Dedmon", "Derrick Favors",
+    "Ausar Thompson", "Jonathan Isaac", "Thanasis Antetokounmpo",
+    "Omer Yurtseven", "Tony Bradley", "Usman Garuba",
+    "Terry Taylor"
+)
+
+# Create a dataframe
+df5 <- data.frame(
+    player = players5,
+    label = "Interior Connector"
+)
+
+# Your text as a character vector
+players6 <- c(
+    "Fred VanVleet", "Tyrese Haliburton",
+    "Jrue Holiday", "Coby White",
+    "Dennis Schroder", "CJ McCollum",
+    "Tyrese Maxey", "Derrick White",
+    "D'Angelo Russell", "Mike Conley",
+    "Caris LeVert", "Kyle Lowry",
+    "Reggie Jackson", "Immanuel Quickley",
+    "Spencer Dinwiddie", "Tyus Jones",
+    "Anfernee Simons", "Malik Monk",
+    "Marcus Smart", "Austin Reaves",
+    "Malcolm Brogdon", "De'Anthony Melton",
+    "Nickeil Alexander-Walker", "Monte Morris",
+    "Devonte' Graham", "Delon Wright",
+    "Payton Pritchard", "Davion Mitchell",
+    "Joe Ingles", "Shake Milton",
+    "Cameron Payne", "Gabe Vincent",
+    "Cory Joseph", "Aaron Holiday",
+    "Andrew Nembhard", "Tre Mann",
+    "Eric Bledsoe", "Jose Alvarado",
+    "Jordan McLaughlin", "Raul Neto",
+    "Lonzo Ball", "Bones Hyland",
+    "Malachi Flynn", "Ty Jerome",
+    "George Hill", "Keyonte George",
+    "Goran Dragic", "Facundo Campazzo",
+    "Brandin Podziemski", "Kemba Walker",
+    "Victor Oladipo", "Lou Williams",
+    "D.J. Augustin", "Kira Lewis Jr.",
+    "Frank Ntilikina", "Marcus Sasser",
+    "Trey Burke", "Skylar Mays"
+)
+
+# Create a dataframe
+df6 <- data.frame(
+    player = players6,
+    label = "Perimeter Anchor"
+)
+
+# Your text as a character vector
+players7 <- c(
+    "Harrison Barnes", "Royce O'Neale", "Dorian Finney-Smith",
+    "P.J. Washington", "Jaden McDaniels", "Isaac Okoro",
+    "OG Anunoby", "Kelly Oubre Jr.", "Terance Mann",
+    "Grant Williams", "Ayo Dosunmu", "Nicolas Batum",
+    "Al Horford", "Herbert Jones", "Caleb Martin",
+    "Patrick Williams", "Alex Caruso", "Jeff Green",
+    "Taurean Prince", "P.J. Tucker", "Matisse Thybulle",
+    "Patrick Beverley", "Derrick Jones Jr.", "Torrey Craig",
+    "Robert Covington", "Aaron Nesmith", "Obi Toppin",
+    "Josh Green", "Naji Marshall", "Kenrich Williams",
+    "Maxi Kleber", "John Konchar", "Dean Wade",
+    "Troy Brown Jr.", "Jalen McDaniels", "Cody Martin",
+    "Josh Okogie", "Aaron Wiggins", "Cam Reddish",
+    "Chuma Okeke", "Oshae Brissett", "Christian Braun",
+    "Ochai Agbaji", "Ziaire Williams", "Javonte Green",
+    "Lamar Stevens", "Dyson Daniels", "Nassir Little",
+    "Garrett Temple", "Haywood Highsmith", "Danuel House Jr.",
+    "Moses Moody", "Juan Toscano-Anderson", "Jeremiah Robinson-Earl",
+    "David Roddy", "Gary Payton II", "Stanley Johnson",
+    "Yuta Watanabe", "Jaime Jaquez Jr.", "James Johnson",
+    "Toumani Camara", "Bilal Coulibaly", "Peyton Watson",
+    "Kevin Knox II", "Markieff Morris", "Andre Iguodala",
+    "JT Thor", "DeAndre' Bembry", "Juancho Hernangomez",
+    "Max Christie", "Romeo Langford", "Sterling Brown",
+    "Kent Bazemore", "Jake LaRavia", "Anthony Black",
+    "Bryce McGowens", "Kessler Edwards", "Solomon Hill",
+    "Rodney Hood", "Maurice Harkless", "Anthony Lamb",
+    "Kris Murray", "Edmond Sumner", "Vince Williams Jr.",
+    "PJ Dozier", "Vit Krejci", "Vlatko Cancar",
+    "CJ Elleby", "Semi Ojeleye", "Nikola Jovic",
+    "MarJon Beauchamp", "Ish Wainright", "Trevor Ariza",
+    "Jalen Wilson", "Dalen Terry", "Dante Exum",
+    "Johnny Davis", "Ousmane Dieng", "Joshua Primo"
+)
+
+# Create a dataframe
+df7 <- data.frame(
+    player = players7,
+    label = "Versatile Connector"
+)
+
+# Your text as a character vector
+players8 <- c(
+    "Buddy Hield", "Kentavious Caldwell-Pope",
+    "Kevin Huerter", "Malik Beasley",
+    "Tim Hardaway Jr.", "Grayson Allen",
+    "Donte DiVincenzo", "Duncan Robinson",
+    "Georges Niang", "Cameron Johnson",
+    "Pat Connaughton", "Reggie Bullock Jr.",
+    "Max Strus", "Corey Kispert",
+    "Keegan Murray", "Justin Holiday",
+    "Cedi Osman", "Luke Kennard",
+    "Gary Harris", "Trey Murphy III",
+    "Doug McDermott", "Patty Mills",
+    "Jae Crowder", "Garrison Mathews",
+    "Amir Coffey", "Jevon Carter",
+    "Quentin Grimes", "Landry Shamet",
+    "Isaiah Joe", "Joe Harris",
+    "Damion Lee", "Sam Hauser",
+    "Danny Green", "Davis Bertans",
+    "Wesley Matthews", "Austin Rivers",
+    "Svi Mykhailiuk", "Miles McBride",
+    "Bryn Forbes", "Simone Fontecchio",
+    "Mike Muscala", "Julian Champagnie",
+    "Cason Wallace", "Ben McLemore",
+    "Isaiah Livers", "Avery Bradley",
+    "Frank Jackson", "Gradey Dick",
+    "Sam Merrill", "Wayne Ellington",
+    "Tony Snell", "Timothe Luwawu-Cabarrot",
+    "Caleb Houstan", "Lindy Waters III",
+    "Keon Ellis", "Rodney McGruder",
+    "Armoni Brooks", "AJ Green",
+    "Ben Sheppard"
+)
+
+# Create a dataframe
+df8 <- data.frame(
+    player = players8,
+    label = "Perimeter Finisher"
+)
+
+# Your text as a character vector
+players9 <- c(
+    "Rudy Gobert", "Ivica Zubac", "Clint Capela",
+    "Jakob Poeltl", "Mason Plumlee", "Daniel Gafford",
+    "Andre Drummond", "Mitchell Robinson", "Steven Adams",
+    "Jalen Duren", "Robert Williams III", "Walker Kessler",
+    "Bismack Biyombo", "DeAndre Jordan", "Tristan Thompson",
+    "JaVale McGee", "Isaiah Jackson", "Jericho Sims",
+    "Dwight Howard", "Willy Hernangomez", "Day'Ron Sharpe",
+    "Cody Zeller", "Moses Brown", "Dereck Lively II",
+    "Hassan Whiteside", "Bruno Fernando", "Trayce Jackson-Davis",
+    "Mark Williams"
+)
+
+# Create a dataframe
+df9 <- data.frame(
+    player = players9,
+    label = "Interior Anchor"
+)
+
+# Your text as a character vector
+players10 <- c(
+    "Domantas Sabonis", "Nikola Jokic", "Julius Randle",
+    "Bam Adebayo", "Giannis Antetokounmpo", "Jonas Valanciunas",
+    "Anthony Davis", "Karl-Anthony Towns", "Joel Embiid",
+    "Kristaps Porzingis", "Alperen Sengun", "Jusuf Nurkic",
+    "Zion Williamson", "Victor Wembanyama", "DeMarcus Cousins"
+)
+
+# Create a dataframe
+df10 <- data.frame(
+    player = players10,
+    label = "Interior Engine"
+)
+
+
+library("tidyverse")
+
+cluster_details <- data.frame(
+    cluster = as.character(c(9, 5, 10, 6, 4, 8, 1, 7, 3, 2)),
+    label = c(
+        "Interior Anchor", "Interior Connector", "Interior Engine", "Perimeter Anchor", "Perimeter Engine",
+        "Perimeter Finisher", "Versatile Anchor", "Versatile Connector", "Versatile Engine", "Versatile Finisher"
+    ),
+    abbrev = c(
+        "IA", "IC", "IE", "PA", "PE", "PF", "VA", "VC", "VE", "VF"
+    )
+)
+
+
+full_df <-
+    bind_rows(
+        df1, df2, df3, df4, df5,
+        df6, df7, df8, df9, df10
+    ) |>
+    inner_join(
+        cluster_details
+    ) |>
+    rename(cluster_name = cluster)
+
+
+
+full_df |>
+    group_by(label, abbrev) |>
+    count() |>
+    ungroup() |>
+    mutate(perc = n / sum(n)) |>
+    arrange(label)
+
+
+nba_adjusted_full <- nba_adjusted |>
+    bind_cols(nba_ids) |>
+    inner_join(
+        full_df,
+        by = join_by(profile_name == player)
+    )
+
+
+write.csv(nba_adjusted_full, "nba-player-clustered.csv", row.names = FALSE)
